533f7bda449fdd4d975dc4cce2a6d11fbc5c9b9f
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2023-09-29 11:27:51 +0100

    VEIOSS-142 | Correct assets files incorrectly rebased.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,121 +1,125 @@
 // prevent resubmit warning
 if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
-  window.history.replaceState(null, null, window.location.href);
+    window.history.replaceState(null, null, window.location.href);
 }
 
+document.addEventListener('DOMContentLoaded', function (event) {
+
 // handle back click
-const backLink = document.querySelector(".govuk-back-link")
-if(backLink) {
-  backLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    window.history.back();
-  });
-}
+    const backLink = document.querySelector(".govuk-back-link")
+    if (backLink) {
+        backLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            window.history.back();
+        });
+    }
 
 // Find first ancestor of el with tagName
 // or undefined if not found
-function upTo(el, tagName) {
-  tagName = tagName.toLowerCase();
+    function upTo(el, tagName) {
+        tagName = tagName.toLowerCase();
 
-  while (el && el.parentNode) {
-    el = el.parentNode;
-    if (el.tagName && el.tagName.toLowerCase() === tagName) {
-      return el;
-    }
-  }
-
-  // Many DOM methods return null if they don't
-  // find the element they are searching for
-  // It would be OK to omit the following and just
-  // return undefined
-  return null;
-}
-
-if (typeof accessibleAutocomplete != 'undefined' && document.querySelector('.autocomplete') != null) {
-  // load autocomplete
-  var selectEl = document.querySelector('.autocomplete');
-  if (selectEl && selectEl.style.display !== "none") {
-    accessibleAutocomplete.enhanceSelectElement({
-      autoselect: true,
-      id: selectEl.id, // Important that id is the same
-      defaultValue: "",
-      minLength: 2,
-      selectElement: selectEl,
-      showAllValues: true
-    });
-  }
-
-  // =====================================================
-  // Polyfill autocomplete once loaded
-  // =====================================================
-  var checkForLoad = setInterval(checkForAutocompleteLoad, 500);
-  var originalSelect = document.querySelector('select.autocomplete');
-  var parentForm = upTo(originalSelect, 'form');
-
-  function polyfillAutocomplete(){
-    var combo = parentForm.querySelector('[role="combobox"]');
-    // =====================================================
-    // Update autocomplete once loaded with fallback's aria attributes
-    // Ensures hint and error are read out before usage instructions
-    // =====================================================
-    if(originalSelect && originalSelect.getAttribute('aria-describedby') > ""){
-      if(parentForm){
-        if(combo){
-          combo.setAttribute('aria-describedby', originalSelect.getAttribute('aria-describedby') + ' ' + combo.getAttribute('aria-describedby'));
-        }
-      }
-    }
-
-    // =====================================================
-    // Ensure when user replaces valid answer with a non-valid answer, then valid answer is not retained
-    // =====================================================
-    var holdSubmit = true;
-    parentForm.addEventListener('submit', function(e){
-      if(holdSubmit){
-        e.preventDefault()
-        if(originalSelect.querySelectorAll('[selected]').length > 0 || originalSelect.value > ""){
-
-          var resetSelect = false;
-
-          if(originalSelect.value){
-            if(combo.value !== originalSelect.querySelector('option[value="' + originalSelect.value +'"]').text){
-              resetSelect = true;
+        while (el && el.parentNode) {
+            el = el.parentNode;
+            if (el.tagName && el.tagName.toLowerCase() === tagName) {
+                return el;
             }
-          }
-          if(resetSelect){
-            originalSelect.value = "";
-            if(originalSelect.querySelectorAll('[selected]').length > 0){
-              originalSelect.querySelectorAll('[selected]')[0].removeAttribute('selected');
-            }
-          }
         }
 
-        holdSubmit = false;
-        //parentForm.submit();
-        HTMLFormElement.prototype.submit.call(parentForm); // because submit buttons have id of "submit" which masks the form's natural form.submit() function
-      }
-    })
-
-  }
-  function checkForAutocompleteLoad(){
-    if(parentForm.querySelector('[role="combobox"]')){
-      clearInterval(checkForLoad)
-      polyfillAutocomplete();
+        // Many DOM methods return null if they don't
+        // find the element they are searching for
+        // It would be OK to omit the following and just
+        // return undefined
+        return null;
     }
-  }
-}
 
-function showTheSpinner() {
+    if (typeof accessibleAutocomplete != 'undefined' && document.querySelector('.autocomplete') != null) {
+        // load autocomplete
+        var selectEl = document.querySelector('.autocomplete');
+        if (selectEl && selectEl.style.display !== "none") {
+            accessibleAutocomplete.enhanceSelectElement({
+                autoselect: true,
+                id: selectEl.id, // Important that id is the same
+                defaultValue: "",
+                minLength: 2,
+                selectElement: selectEl,
+                showAllValues: true
+            });
+        }
 
-  //make the spinning wheel visible
-  document.getElementById("processing-wheel").hidden = false;
+        // =====================================================
+        // Polyfill autocomplete once loaded
+        // =====================================================
+        var checkForLoad = setInterval(checkForAutocompleteLoad, 500);
+        var originalSelect = document.querySelector('select.autocomplete');
+        var parentForm = upTo(originalSelect, 'form');
 
-  //make the button hidden
-  document.getElementById("continue").style.visibility = 'hidden';
-  document.getElementById("saveProgress").style.visibility = 'hidden';
+        function polyfillAutocomplete() {
+            var combo = parentForm.querySelector('[role="combobox"]');
+            // =====================================================
+            // Update autocomplete once loaded with fallback's aria attributes
+            // Ensures hint and error are read out before usage instructions
+            // =====================================================
+            if (originalSelect && originalSelect.getAttribute('aria-describedby') > "") {
+                if (parentForm) {
+                    if (combo) {
+                        combo.setAttribute('aria-describedby', originalSelect.getAttribute('aria-describedby') + ' ' + combo.getAttribute('aria-describedby'));
+                    }
+                }
+            }
+
+            // =====================================================
+            // Ensure when user replaces valid answer with a non-valid answer, then valid answer is not retained
+            // =====================================================
+            var holdSubmit = true;
+            parentForm.addEventListener('submit', function (e) {
+                if (holdSubmit) {
+                    e.preventDefault()
+                    if (originalSelect.querySelectorAll('[selected]').length > 0 || originalSelect.value > "") {
+
+                        var resetSelect = false;
+
+                        if (originalSelect.value) {
+                            if (combo.value !== originalSelect.querySelector('option[value="' + originalSelect.value + '"]').text) {
+                                resetSelect = true;
+                            }
+                        }
+                        if (resetSelect) {
+                            originalSelect.value = "";
+                            if (originalSelect.querySelectorAll('[selected]').length > 0) {
+                                originalSelect.querySelectorAll('[selected]')[0].removeAttribute('selected');
+                            }
+                        }
+                    }
+
+                    holdSubmit = false;
+                    //parentForm.submit();
+                    HTMLFormElement.prototype.submit.call(parentForm); // because submit buttons have id of "submit" which masks the form's natural form.submit() function
+                }
+            })
+
+        }
+
+        function checkForAutocompleteLoad() {
+            if (parentForm.querySelector('[role="combobox"]')) {
+                clearInterval(checkForLoad)
+                polyfillAutocomplete();
+            }
+        }
+    }
+
+    function showTheSpinner() {
+
+        //make the spinning wheel visible
+        document.getElementById("processing-wheel").hidden = false;
+
+        //make the button hidden
+        document.getElementById("continue").style.visibility = 'hidden';
+        document.getElementById("saveProgress").style.visibility = 'hidden';
 
 
-  return false;
-}
+        return false;
+    }
+});
 
