11d0a863ceb5ad74e822c691859be341810cff18
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2024-08-20 12:31:01 +0100

    Use cards on check previous scheme CYA (#129)
    * Use cards on check previous scheme CYA
    
    * Add missing messages and remove duplicated row
    
    * fix tests

--- app/viewmodels/checkAnswers/previousRegistrations/PreviousSchemeSummary.scala
+++ app/viewmodels/checkAnswers/previousRegistrations/PreviousSchemeSummary.scala
@@ -17,42 +17,55 @@
 package viewmodels.checkAnswers.previousRegistrations
 
 import models.{Country, Index, PreviousScheme, UserAnswers}
+import models.previousRegistrations.SchemeDetailsWithOptionalVatNumber
+import models.requests.AuthenticatedDataRequest
 import pages.Waypoints
 import pages.previousRegistrations.PreviousSchemePage
 import play.api.i18n.Messages
 import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
-import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist._
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
 
-object PreviousSchemeSummary  {
+object PreviousSchemeSummary {
 
-  def row(answers: UserAnswers, countryIndex: Index, schemeIndex: Index, country: Country, existingPreviousSchemes: Seq[PreviousScheme], waypoints: Waypoints)
-         (implicit messages: Messages): Option[SummaryListRow] =
-    answers.get(PreviousSchemePage(countryIndex, schemeIndex)).map {
-      previousAnsweredScheme: PreviousScheme =>
+  def getSummaryLists(
+                       previousSchemes: List[SchemeDetailsWithOptionalVatNumber],
+                       countryIndex: Index,
+                       country: Country,
+                       existingSchemes: Seq[PreviousScheme],
+                       waypoints: Waypoints
+                     )(implicit request: AuthenticatedDataRequest[_], messages: Messages): List[SummaryList] = {
 
-        val isExistingScheme = existingPreviousSchemes.contains(previousAnsweredScheme)
 
-        val value = ValueViewModel(
-          HtmlContent(
-            HtmlFormat.escape(messages(s"previousScheme.$previousAnsweredScheme"))
+    previousSchemes.zipWithIndex.flatMap { case (scheme, schemeIndex) =>
+      request.userAnswers.get(PreviousSchemePage(countryIndex, Index(schemeIndex))).map { previousAnsweredScheme: PreviousScheme =>
+        val isExistingScheme = existingSchemes.contains(previousAnsweredScheme)
+        SummaryListViewModel(
+          rows = Seq(
+            PreviousSchemeNumberSummary.row(request.userAnswers, countryIndex, Index(schemeIndex), scheme.previousScheme),
+            PreviousIntermediaryNumberSummary.row(request.userAnswers, countryIndex, Index(schemeIndex))
+          ).flatten
+        ).withCard(
+          card = Card(
+            title = Some(CardTitle(content = HtmlContent(HtmlFormat.escape(messages(s"previousScheme.$previousAnsweredScheme"))))),
+            actions = Some(Actions(
+              items = if (!isExistingScheme) {
+                Seq(
+                  ActionItemViewModel(
+                    "site.remove",
+                    controllers.previousRegistrations.routes.DeletePreviousSchemeController.onPageLoad(waypoints, countryIndex, Index(schemeIndex)).url
+                  ).withVisuallyHiddenText(messages("site.remove.hidden", country.name, HtmlFormat.escape(messages(s"previousScheme.$previousAnsweredScheme"))))
+                )
+              } else {
+                Seq.empty
+              }
+            ))
           )
         )
-
-        SummaryListRowViewModel(
-          key     = "previousScheme.checkYourAnswersLabel",
-          value   = value,
-          actions = if (!isExistingScheme) {
-            Seq(
-              ActionItemViewModel("site.remove", controllers.previousRegistrations.routes.DeletePreviousSchemeController.onPageLoad(
-                waypoints, countryIndex, schemeIndex).url)
-                .withVisuallyHiddenText(messages("site.remove.hidden", country.name, HtmlFormat.escape(messages(s"previousScheme.$previousAnsweredScheme"))))
-            )
-          } else {
-            Seq.empty
-          }
-        )
+      }
     }
+  }
+
 }
