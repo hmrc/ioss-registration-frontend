1857967310d5ac8988255276c7752a56c85f433b
Lee Powell <122445508+Lee-Powell@users.noreply.github.com>
2024-09-09 13:56:52 +0100

    VEIOSS-658 | added Expired to Submission Status to fix issue with rejoin link (#134)

--- app/models/SubmissionStatus.scala
+++ app/models/SubmissionStatus.scala
@@ -30,7 +30,9 @@ object SubmissionStatus extends Enumerable.Implicits {
 
   case object Excluded extends WithName("EXCLUDED") with SubmissionStatus
 
-  val values: Seq[SubmissionStatus] = Seq(Due, Overdue, Complete, Next, Excluded)
+  case object Expired extends WithName("EXPIRED") with SubmissionStatus
+
+  val values: Seq[SubmissionStatus] = Seq(Due, Overdue, Complete, Next, Excluded, Expired)
 
   implicit val enumerable: Enumerable[SubmissionStatus] =
     Enumerable(values.map(v => v.toString -> v): _*)
