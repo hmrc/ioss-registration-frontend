41ccbe9473b37c14f569187eaa4b222121a717b9
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2024-10-10 13:27:20 +0100

    VEIOSS-697 | Update library and app dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,16 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
-  private val hmrcMongoVersion = "1.7.0"
+  private val bootstrapVersion = "9.5.0"
+  private val hmrcMongoVersion = "2.2.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.5.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.13.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "domain-play-30"                         % "9.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                         % "10.0.0",
     "org.typelevel"     %% "cats-core"                              % "2.9.0"
   )
 
