b55f7d13897086571284a240955b0c4901949bd2
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2025-02-18 12:37:37 +0000

    VEIOSS-750 | Implement content design changes. (#160)
    * VEIOSS-750 | Implement content design changes.
    
    * VEIOSS-750 | Update dependencies as per Bobby warnings.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.5.0"
-  private val hmrcMongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.9.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
