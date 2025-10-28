cb67d3418696cff81c1c685373fb69150db6d374
DevAndyroo <andy@andyroo.info>
2025-10-28 18:06:36 +0000

    up version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcMongoVersion = "2.10.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
