a8e5c30b2aebd097644f3be58272336dbe3253df
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2026-07-29 08:29:23 +0100

    VEIOSS-915 | Update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
